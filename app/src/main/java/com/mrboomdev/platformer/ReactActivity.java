f0a0d4b61a3502442e19fc991d5eda04f3e85fd5
MrBoomDev <github@mrboomdev.ru>
2023-01-11 02:12:07 +0500

    настроил react

--- app/src/main/java/com/mrboomdev/platformer/ReactActivity.java
+++ app/src/main/java/com/mrboomdev/platformer/ReactActivity.java
@@ -1,28 +1,54 @@
 package com.mrboomdev.platformer;
 
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.common.LifecycleState;
+import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import com.mrboomdev.platformer.MainGame;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.soloader.SoLoader;
 
-public class ReactActivity extends Activity {
+public class ReactActivity extends Activity implements DefaultHardwareBackBtnHandler {
+    private ReactRootView root;
+    private ReactInstanceManager instance;
 
   @Override
   protected void onCreate(Bundle bundle) {
     super.onCreate(bundle);
-    //SoLoader.init()
-    Button button = new Button(this);
+    SoLoader.init(this, false);
+    root = new ReactRootView(this);
+    List<ReactPackage> packages = new PackageList(getApplication()).getPackages();
+    instance = ReactInstanceManager.builder()
+        .setApplication(getApplication())
+        .setCurrentActivity(this)
+        .setBundleAssetName("index.android.bundle")
+        .setJSMainModulePath("index")
+        .addPackages(packages)
+        .setUseDeveloperSupport(BuildConfig.DEBUG)
+        .setInitialLifecycleState(LifecycleState.RESUMED)
+        .build();
+    root.startReactApplication(instance, "GameLobbyScreen", null);
+    setContentView(root);
+    /*Button button = new Button(this);
     button.setText("Open game");
-    button.setOnClickListener((View view) -> {
-        finish();
-        MainGame.getInstance().toggleGameView(true);
-    });
-    setContentView(button);
+    button.setOnClickListener(
+        (View view) -> {
+          finish();
+          MainGame.getInstance().toggleGameView(true);
+        });
+    setContentView(button);*/
   }
 
   @Override
   public void onBackPressed() {
     super.onBackPressed();
+  }
+
+  @Override
+  public void invokeDefaultOnBackPressed() {
+      this.onBackPressed();
   }
 }

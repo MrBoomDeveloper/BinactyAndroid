3ca51d9c799bfb92b65124240b62f71dbe57cf48
MrBoomDev <github@mrboomdev.ru>
2023-01-12 03:38:31 +0500

    добавил активити быстрого запуска тестовой сцены

--- README.md
+++ README.md
@@ -1,7 +1,47 @@
+###### Documentation was written only for Russian, please use Google Translate.
 ### Об проекте
 Данную игру я разрабатываю, дабы отточить свои навыки в архитектуре, дизайне и других сферах.
 Здесь когда-нибудь будет сюдет, хорошая графика, оптимизация.
 По сюжету богатые люди заскучали и решили сыграть в игру. Игра, в которой главная цель насладиться смертью других. Кого-то охватывает страх, отчаяние, уныние, а кто-то уже сошел с ума и воцаряер хаос.
+### Plans
+- Gameplay:
+  - Advanced bots ai:
+    - Stuck position escaping
+    - Try to defense
+    - Random glitches to look like a human
+    - Random personalities
+  - More maps
+  - More characters
+  - Actions (Attack, dash, shoot, heal, super)
+  - Effects (Fire, boom and etc.)
+  - Sounds (Footsteps, effects and etc.)
+- Multiplayer mode:
+  - Room hosting
+  - Room joining
+  - Rooms lost provider
+- Global improving system:
+  - Weekly missions
+  - Skills tree-style upgrading
+- Story mode:
+  - Intro
+  - Dialogs
+  - Ending
+- Fancy react menu:
+  - Main screen
+  - Shop screen
+  - Achievments screen
+  - Settings screen
+  - Profile overview
+  - GameOver screen
+- Internal mods support:
+  - Map Editor
+  - Character editor
+  - Detailed character editor (with custom bones, animations)
+- Gamejolt linking:
+  - Achievments
+  - Account sync
+  - Leaderboard
+  - Friends system
 ### Сторонние ссылки
 https://gamejolt.com/games/actionplatformer/670228
 ### Сборка проектa
---
+++ app/src/main/java/com/mrboomdev/platformer/DebugStartGameActivity.java
@@ -0,0 +1,49 @@
+package com.mrboomdev.platformer;
+
+import android.os.Bundle;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.mrboomdev.platformer.MainGame;
+import com.mrboomdev.platformer.NativeContainer;
+import com.itsaky.androidide.logsender.LogSender;
+import com.mrboomdev.platformer.scenes.loading.LoadingScreen;
+
+public class DebugStartGameActivity extends AndroidApplication implements NativeContainer {
+    private FirebaseAnalytics analytics;
+    private AndroidApplicationConfiguration gameConfig;
+    private boolean isInitialized = false;
+    
+	@Override
+	protected void onCreate(Bundle instance) {
+		super.onCreate(instance);
+		LogSender.startLogging(this);
+        analytics = FirebaseAnalytics.getInstance(this);
+        
+		gameConfig = new AndroidApplicationConfiguration();
+		gameConfig.useImmersiveMode = true;
+		gameConfig.useAccelerometer = false;
+		gameConfig.useCompass = false;
+        
+    toggleGameView(true);
+	}
+    
+    @Override
+    public void toggleGameView(boolean isActive) {
+        if(isActive) {
+            if(!isInitialized) {
+                initialize(MainGame.getInstance(new AndroidAnalytics(), this), gameConfig);
+                isInitialized = true;
+            } else {
+                Gdx.app.postRunnable(() -> {
+			        MainGame.getInstance().setScreen(new LoadingScreen(LoadingScreen.LoadScene.GAMEPLAY));
+                });
+            }
+        } else {
+	  	    Gdx.app.postRunnable(() -> {
+			    MainGame.getInstance().setScreen(new LoadingScreen(LoadingScreen.LoadScene.GAMEPLAY));
+            });
+        }
+    }
+}
